8e19298b67dbcf7f8fda21d35120d77088e04a62
Iordanis Kostelidis <ikostelidis@kostelidis.dev>
2024-09-28 11:27:40 +0300

    2nd Implementation, 50%

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+cache = {}
+
 def calculate(stairs: int) -> int:
+    if stairs in cache:
+        return cache[stairs]
     if stairs == 0:
         return 0
     if stairs == 1 or stairs == 2:
@@ -6,6 +10,9 @@ def calculate(stairs: int) -> int:
     return calculate(stairs - 1) + calculate(stairs - 2)
 
 def main():
+    cases_to_cache = range(1, 20)
+    for case_to_cache in cases_to_cache:
+        cache[case_to_cache] = calculate(case_to_cache)
     for test_case in range(int(input())):
         print(calculate(int(input())+1))
 
